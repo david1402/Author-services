0998f81732ae89b3cd8c0d53632897024367c25a
David <49765318+david1402@users.noreply.github.com>
2021-08-16 13:11:45 +0200

    Add files via upload

---
+++ BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
